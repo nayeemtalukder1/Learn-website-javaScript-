ac7874806fc85ee8928535f0e555848b596a20f3
nayeemtalukder1 <nayeemtalukder882@gmail.com>
2023-12-29 23:29:41 +0600

    add css and complite project

--- Todo List Project/todolist.js
+++ Todo List Project/todolist.js
@@ -1,49 +1,38 @@
 let todoList = [
-  
   {
-    item: 'Buy Milk', 
+    item: 'Buy Milk',
+    dueDate: '4/10/2023'
+  },
+  {
+    item: 'Go to College',
     dueDate: '4/10/2023'
   }
 ];
+
 displayItems();
+
 function addTodo() {
   let inputElement = document.querySelector('#todo-input');
   let dateElement = document.querySelector('#todo-date');
   let todoItem = inputElement.value;
   let todoDate = dateElement.value;
-  console.log(todoItem);
-
   todoList.push({item: todoItem, dueDate: todoDate});
   inputElement.value = '';
   dateElement.value = '';
-
   displayItems();
 }
 
 function displayItems() {
   let containerElement = document.querySelector('.todo-container');
-
-
   let newHtml = '';
-
-  
   for (let i = 0; i < todoList.length; i++) {
-    //let Item = todoList[i].item;
-    //let dueDate = todoList[i].dueDate;
-    //object destructuring
-    let {item,dueDate} = todoList[i];
-    newHtml+= `
-    <div>
-    <span>${Item}</span>
-    <span>${dueDate}</span>
-    <button onclick="todoList.splice(${i},1);
-    displayItems();
-    ">Delete</button>
-    </div>
-    `
-    
+    let {item, dueDate} = todoList[i];
+    newHtml += `
+      <span>${item}</span>
+      <span>${dueDate}</span>
+      <button class='btn-delete' onclick="todoList.splice(${i}, 1);
+      displayItems();">Delete</button>
+    `;
   }
- containerElement.innerHTML = newHtml;
-
-   
+  containerElement.innerHTML = newHtml;
 }
